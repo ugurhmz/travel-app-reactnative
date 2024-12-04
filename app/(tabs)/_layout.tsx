a1470136b00f096221bc42ee4e33b48139e95163
Ugur hamzaoglu <ugur.hamzaoglu@pozitiftech.com>
2024-12-05 01:33:52 +0300

    Home searchbar and Tabbar configure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,10 +2,22 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
 import { FontAwesome, Ionicons, MaterialIcons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 export default function Layout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: Colors.bgColor,
+          borderTopWidth: 0,
+          padding: 0,
+        },
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: Colors.black,
+        tabBarInactiveTintColor: "#999",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
@@ -22,11 +34,32 @@ export default function Layout() {
           ),
         }}
       />
+
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <View
+              style={{
+                backgroundColor: Colors.primaryColor,
+                width: 70,
+                borderRadius: 10,
+                height: 50,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Ionicons name="search-outline" size={28} color={Colors.white} />
+            </View>
+          ),
+        }}
+      />
+
       <Tabs.Screen
         name="bookmarks"
         options={{
           tabBarIcon: ({ color }) => (
-            <Ionicons name="bookmarks" size={28} color={color} />
+            <Ionicons name="bookmark" size={28} color={color} />
           ),
         }}
       />
@@ -35,14 +68,6 @@ export default function Layout() {
         options={{
           tabBarIcon: ({ color }) => (
             <FontAwesome name="user" size={28} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="search" size={28} color={color} />
           ),
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,76 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+import { useHeaderHeight } from "@react-navigation/elements";
 
 const index = () => {
+  const headerHeight = useHeaderHeight();
+
   return (
-    <View style={styles.container}>
-      <Text>index</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {}} style={{ marginLeft: 20 }}>
+              <Image
+                source={{
+                  uri: "https://randomuser.me/api/portraits/women/79.jpg",
+                }}
+                style={{ width: 40, height: 40, borderRadius: 10 }}
+              />
+            </TouchableOpacity>
+          ),
+
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                marginRight: 20,
+                backgroundColor: Colors.white,
+                padding: 10,
+                borderRadius: 10,
+                shadowColor: "#171717",
+                shadowOffset: { width: 2, height: 4 },
+                shadowOpacity: 0.2,
+                shadowRadius: 3,
+              }}
+            >
+              <Ionicons name="notifications" size={20} color={Colors.black} />
+            </TouchableOpacity>
+          ),
+        }}
+      ></Stack.Screen>
+      <View style={[styles.container, { paddingTop: headerHeight }]}>
+        <Text style={styles.headingText}>Lorem ipmsum doarl qwekjsadk</Text>
+
+        <View style={styles.searchSectionWrapper}>
+          <View style={styles.searchBar}>
+            <Ionicons
+              name="search"
+              size={18}
+              style={{ marginRight: 5 }}
+              color={Colors.black}
+            />
+            <TextInput placeholder="Search..."></TextInput>
+          </View>
+
+          <TouchableOpacity onPress={() => {}} style={styles.filterBtn}>
+            <Ionicons name="options" size={28} color={Colors.white} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   );
 };
 
@@ -14,9 +79,31 @@ export default index;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 1,
-    marginBottom: 0,
+    paddingHorizontal: 20,
+    backgroundColor: Colors.bgColor,
+  },
+  headingText: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: Colors.black,
+    marginTop: 12,
+  },
+  searchSectionWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
+  },
+  searchBar: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: Colors.white,
+    padding: 16,
+    borderRadius: 10,
+  },
+
+  filterBtn: {
+    backgroundColor: Colors.primaryColor,
+    padding: 12,
+    borderRadius: 10,
+    marginLeft: 20,
   },
 });
